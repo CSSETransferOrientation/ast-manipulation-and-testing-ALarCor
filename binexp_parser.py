b0ae9fe2bfe74c05904714e168307d513d1b596a
Stephen R. Beard <srbeard@calpoly.edu>
2024-09-02 15:00:49 -0700

    Graded, see ;;> lines for comments

--- binexp_parser.py
+++ binexp_parser.py
@@ -80,6 +80,7 @@ class BinOpAst():
         Reduce additive identities
         x + 0 = x
         """
+        # ;;> This doesn't recur... 
         if self.type == NodeType.operator and self.val == '+':
             if self.left.type == NodeType.number and self.left.val == '0':
                 return self.right
@@ -92,6 +93,7 @@ class BinOpAst():
         Reduce multiplicative identities
         x * 1 = x
         """
+        # ;;> This doesn't recur... 
         if self.type == NodeType.operator and self.val == '*':
             if self.left.type == NodeType.number and self.left.val == '1':
                 return self.right
@@ -104,6 +106,7 @@ class BinOpAst():
         Reduce multiplication by zero
         x * 0 = 0
         """
+        # ;;> This doesn't recur... 
         if self.type == NodeType.operator and self.val == '*':
             if self.left.type == NodeType.number and self.left.val == '0':
                 return BinOpAst(['0'])
@@ -119,6 +122,7 @@ class BinOpAst():
         3) Extra #1: Multiplication by 0, e.g. x * 0 = 0
         4) Extra #2: Constant folding, e.g. statically we can reduce 1 + 1 to 2, but not x + 1 to anything
         """
+        # ;;> Ahhh, you were relying on simplify for the recursion... that doesn't match the spec but this does work
         self.left = self.left.simplify_binops() if self.left else None
         self.right = self.right.simplify_binops() if self.right else None
         self = self.additive_identity()
@@ -138,6 +142,7 @@ if __name__ == "__main__":
         (['+', '1', '*', '0', '1'], '1')
     ]
 
+ # ;;> This does not use the file based testing required in the assignment!
     for test_input, expected_output in tests:
         ast = BinOpAst(test_input)
         simplified_ast = ast.simplify_binops()
